f16fc9fec8f75eab570cb1bf5c446c5bbe9dec7e
Kopalmuhamad <muhamadkopal32@gmail.com>
2024-01-11 02:05:02 +0700

    fixed link

--- src/libs/projectData.js
+++ src/libs/projectData.js
@@ -5,8 +5,12 @@ import Furnuture from "../assets/furnuture.webp";
 import CloverPerfume from "../assets/clover-perfume.webp";
 
 export const projectData = [
-  { name: "Apartreal", img: Apartreal, href: "https://apartreal.vercel.app" },
-  { name: "Movie Rated", img: MovieRated, href: "movie-rated.vercel.app" },
+  { name: "Apartreal", img: Apartreal, href: "https://apart-real.vercel.app" },
+  {
+    name: "Movie Rated",
+    img: MovieRated,
+    href: "https://movie-rated.vercel.app",
+  },
   {
     name: "Tabungan Bersama",
     img: TabunganBersama,
